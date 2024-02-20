fecb23eaeb73d0b50621ae8875af6c49ac83e171
Aviral Saxena <64943664+AviralSaxena@users.noreply.github.com>
2024-02-19 22:21:18 -0600

    Tests 1 to 6

--- msh/msh.c
+++ msh/msh.c
@@ -31,8 +31,113 @@
 #include <string.h>
 
 
-int main( int argc, char * argv[] )
-{
-  return 0;
-}
+#define MAX_COMMAND_SIZE 255
+#define MAX_NUM_ARGUMENTS 10
 
+int main(int argc, char *argv[]) {
+    char *cmd = (char *)malloc(MAX_COMMAND_SIZE);
+    char *tokens[MAX_NUM_ARGUMENTS];
+    FILE *input_stream = stdin;
+
+    char error_message[30] = "An error has occurred\n";
+
+    // Batch mode
+    if (argc == 2) {
+        input_stream = fopen(argv[1], "r");
+        if (input_stream == NULL) {
+            fprintf(stderr, "Error opening file: %s\n", argv[1]);
+            exit(1);
+        }
+    } else if (argc > 2) {
+        fprintf(stderr, "Usage: msh [batchFile]\n");
+        exit(1);
+    }
+
+    while (1) {
+
+        if (input_stream == stdin) {
+            printf("msh> ");
+        }
+
+        if (fgets(cmd, MAX_COMMAND_SIZE, input_stream) == NULL) {
+            // End of file (Ctrl+D)
+            break;
+        }
+
+        // Parse input
+        int token_count = 0;
+        char *arg_ptr;
+        char *working_str = strdup(cmd);
+
+        // Tokenize the input string
+        while (((arg_ptr = strsep(&working_str, " \t\n")) != NULL) && (token_count < MAX_NUM_ARGUMENTS)) {
+            tokens[token_count] = strndup(arg_ptr, MAX_COMMAND_SIZE);
+            if (strlen(tokens[token_count]) == 0) {
+                free(tokens[token_count]);
+            } else {
+                token_count++;
+            }
+        }
+
+        // Empty command
+        if (token_count == 0) {
+            free(working_str);
+            continue;
+        }
+
+        // Redirection
+
+        for(int i = 0; i < token_count; i++) {
+          if(strcmp(tokens[i], ">") == 0) {
+              if(i == token_count - 1 || tokens[i+1] == NULL || strcmp(tokens[i+1], ">") == 0) {
+                  write(STDERR_FILENO, error_message, strlen(error_message));
+                  free(working_str);
+                  continue;
+              }
+          }
+        }
+
+        // Built-in commands
+        if (strcmp(tokens[0], "exit") == 0) {
+            if (token_count > 1) {
+                write(STDERR_FILENO, error_message, strlen(error_message));
+                continue;
+            } else {
+                exit(0);
+            }
+        } else if (strcmp(tokens[0], "cd") == 0) {
+            if (token_count != 2) {
+                write(STDERR_FILENO, error_message, strlen(error_message));
+            } else if (chdir(tokens[1]) != 0) {
+                write(STDERR_FILENO, error_message, strlen(error_message));
+            }
+            free(working_str);
+            continue;
+        }
+
+        pid_t pid = fork();
+        if (pid == 0) {
+            // Child process
+            tokens[token_count] = NULL;
+            if (execvp(tokens[0], tokens) == -1) {
+                write(STDERR_FILENO, error_message, strlen(error_message));
+                exit(EXIT_FAILURE);
+            }
+        } else if (pid > 0) {
+            // Parent process
+            wait(NULL);
+        } else {
+            // Fork failed
+            perror("fork");
+        }
+
+        free(working_str);
+    }
+
+    free(cmd);
+    if (input_stream != stdin) {
+        fclose(input_stream);
+    }
+
+    return 0;
+}
